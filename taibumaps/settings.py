5a4c8969f3d44573f8a7d9510a71b7acd9321198
Carlos Lima <eucandre@gmail.com>
2024-08-16 09:14:35 -0300

    Atualizado o redirect para o /

--- taibumaps/settings.py
+++ taibumaps/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/farms/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/accounts/login/' 
 
